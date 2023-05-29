1d2f157f751bce05fbd49c159f7de352db84e20f
Glit <63010022@kmitl.ac.th>
2023-05-29 09:12:52 +0700

    Add Product factory and seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
+
+    protected $model = Product::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,11 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'image' => 'image/product/example.jpg',
+            'price' => random_int(50, 10000),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
